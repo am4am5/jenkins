94b16dd228ac2ea31b1a38b86e0e230c76433fff
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-03-05 15:06:54 +0300

    Add files via upload

---
+++ frontend/build.sh
@@ -0,0 +1,2 @@
+docker build -t 172.16.9.49:30020/front/aismv-front-edms:latest . && docker push 172.16.9.49:30020/front/aismv-front-edms:latest && docker rmi 172.16.9.49:30020/front/aismv-front-edms:latest
+
