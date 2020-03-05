b2bb3d028de4622f8f67b655e1a61fea54e78b94
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-03-05 15:25:35 +0300

    Create build.sh

---
+++ backend/build.sh
@@ -0,0 +1,1 @@
+docker build -t 172.16.9.49:30020/back/aismv-back-edms:latest . && docker push 172.16.9.49:30020/back/aismv-back-edms:latest && docker rmi 172.16.9.49:30020/back/aismv-back-edms:latest
