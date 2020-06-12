0711669be390ea14b44346ddfde8582d10ffca55
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-06-13 02:09:41 +0300

    Update backupconfig.sh

--- backupconfig.sh
+++ backupconfig.sh
@@ -1,1 +1,10 @@
 find -maxdepth 3 -name config.xml|xargs tar czvf /root/jenkins.tar.gz
+
+
+    declare -i j=0
+    for i in $(java -jar jenkins-cli.jar -s http://server:8080/jenkins list-jobs  --username **** --password ***);
+    do
+    java -jar jenkins-cli.jar -s http://lxvbmcbma:8080/jenkins get-job --username **** --password **** ${i} > ${i}.xml;
+    echo "done";
+    fi
+    done
